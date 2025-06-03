ed9f523be2b80233e350b0396c8efd208e178e2a
Heejin <dev.heejiin@gmail.com>
2025-06-04 03:41:58 +0900

    view 로직 기한 수정

--- middleware.ts
+++ middleware.ts
@@ -8,8 +8,7 @@ export function middleware(request: NextRequest) {
     const view = Math.random() < 0.5 ? 'grid' : 'list';
     response.cookies.set('viewMode', view, {
       path: '/',
-      //@TODO: Set a proper expiration date
-      expires: new Date(0),
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
     });
   }
 
